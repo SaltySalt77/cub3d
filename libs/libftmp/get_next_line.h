a952c9028a5aa642ed5e81d215b1ca12f59be67a
Hyeon Na <hyna@c5r8s3.42seoul.kr>
2022-12-27 11:57:23 +0900

    feat : libft 추가함
    디렉토리명 변경했으니 확인 바람

---
+++ libs/libftmp/get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyna <hyna@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/14 18:18:41 by hyna              #+#    #+#             */
+/*   Updated: 2022/12/27 11:47:31 by hyna             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+char	*free_str(char	*s);
+
+#endif
