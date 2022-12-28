9bf066a714f13fa521544d856c5440a084defe29
Namik Hwang <nhwang@c5r1s7.42seoul.kr>
2022-12-28 12:02:53 +0900

    fix : norm fix check_vec

--- srcs/map_parsing/check_vec.c
+++ srcs/map_parsing/check_vec.c
@@ -1,53 +1,55 @@
 #include "cub3d.h"
 
-int check_vec(t_info *info)
+int	check_vec_util(t_info *info, int y, int x)
 {
-	int	y;
-	int	x;
-	int idx;
+	int	idx;
 	int	ny[4];
-	int nx[4];
-	int dy;
-	int dx;
+	int	nx[4];
+	int	dy;
+	int	dx;
 
 	ny[0] = 1;
 	ny[1] = -1;
 	ny[2] = 0;
 	ny[3] = 0;
-
 	nx[0] = 0;
 	nx[1] = 0;
 	nx[2] = 1;
 	nx[3] = -1;
+	idx = 0;
+	while (idx < 4)
+	{
+		dy = ny[idx] + y;
+		dx = nx[idx] + x;
+		if (dy < 0 || dx < 0 || dy >= info->max_hei || dx >= info->max_wid
+			|| info->map[dy][dx] == ' ')
+		{
+			printf("%d, %d : out \n",dy,dx);//
+			return (0);
+		}
+		idx++;
+	}
+	return (1);
+}
+
+int	check_vec(t_info *info)
+{
+	int	y;
+	int	x;
 
 	y = 0;
-	while(info->map[y])
+	while (info->map[y])
 	{
 		x = 0;
-		while(info->map[y][x])
+		while (info->map[y][x])
 		{
-			idx = 0;
-			if (info->map[y][x]!='0')
+			if (info->map[y][x] != '0')
 			{
 				x++;
-				continue;
+				continue ;
 			}
-			while(idx < 4)
-			{
-				dy = ny[idx]+y;
-				dx = nx[idx]+x;
-				if (dy<0 || dx < 0 || dy >= info->max_hei || dx >= info->max_wid)
-				{
-					printf("%d , %d out\n",dy,dx);///
-					return (0);
-				}
-				if (info->map[dy][dx] == ' ')
-				{
-					printf("%d , %d space\n",dy,dx);///
-					return (0);
-				}
-				idx++;
-			}
+			if (!check_vec_util(info, y, x))
+				return (0);
 			x++;
 		}
 		y++;
